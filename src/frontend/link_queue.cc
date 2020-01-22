e3ede266a9f5fe1f40fcecac32464d435e810b88
mickey946 <mickey946@gmail.com>
2020-01-22 13:10:16 +0200

    Link modifications can be done during runtime using a named pipe located in the /tmp folder when the name starts with adv_. Observations are written to another named pipe in the same location with a name that starts with obs_

--- src/frontend/link_queue.cc
+++ src/frontend/link_queue.cc
@@ -2,6 +2,15 @@
 
 #include <limits>
 #include <cassert>
+#include <random>
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
 
 #include "link_queue.hh"
 #include "timestamp.hh"
@@ -18,10 +27,34 @@ LinkQueue::LinkQueue( const string & link_name, const string & filename, const s
     : next_delivery_( 0 ),
       schedule_(),
       base_timestamp_( timestamp() ),
+      bandwidth_history(0),
+      is_adversary_(false),
+      cur_bw_(12000000.0),
+      cur_delay_ms_(10.0),
+      cur_loss_rate_(0.0),
+      packets_this_departure_time_(0),
+      prev_departure_time_(0),
+      next_possible_departure_time_(0),
+      lcm(0),
+      total_bandwidth_cycles(1),
+      total_delivery_cycles(1),
+      packets_ingress_(0),
+      bytes_ingress_(0),
+      packets_egress_(0),
+      bytes_egress_(0),
+      packet_drops_(0),
+      bytes_drops_(0),
+      total_delay_(0),
+      mi_start_time_(timestamp()),
+      mi_start_queue_size_(0),
+      mi_dur_(30),
+      actions_(),
+      got_valid_action_(false),
       packet_queue_( move( packet_queue ) ),
       packet_in_transit_( "", 0 ),
       packet_in_transit_bytes_left_( 0 ),
-      output_queue_(),
+      transit_queue_(),
+      adversary_data_out_(),
       log_(),
       throughput_graph_( nullptr ),
       delay_graph_( nullptr ),
@@ -29,38 +62,47 @@ LinkQueue::LinkQueue( const string & link_name, const string & filename, const s
       finished_( false )
 {
     assert_not_root();
+    
+    if (filename.find("adv", 0) == 0) {
+        is_adversary_ = true;
+        std::string obs_filename = std::string("/tmp/adversary_pipes/").append(filename).append("_obs");
+        std::string ac_filename = std::string("/tmp/adversary_pipes/").append(filename).append("_actions");
+        adversary_data_out_.open(obs_filename);
+        poll_fds_[0].fd = open(ac_filename.c_str(), O_RDONLY | O_NONBLOCK);
+        poll_fds_[0].events = POLLIN;
+    } else {
+        /* open filename and load schedule */
+        ifstream trace_file( filename );
 
-    /* open filename and load schedule */
-    ifstream trace_file( filename );
-
-    if ( not trace_file.good() ) {
-        throw runtime_error( filename + ": error opening for reading" );
-    }
-
-    string line;
-
-    while ( trace_file.good() and getline( trace_file, line ) ) {
-        if ( line.empty() ) {
-            throw runtime_error( filename + ": invalid empty line" );
+        if ( not trace_file.good() ) {
+            throw runtime_error( filename + ": error opening for reading" );
         }
 
-        const uint64_t ms = myatoi( line );
+        string line;
 
-        if ( not schedule_.empty() ) {
-            if ( ms < schedule_.back() ) {
-                throw runtime_error( filename + ": timestamps must be monotonically nondecreasing" );
+        while ( trace_file.good() and getline( trace_file, line ) ) {
+            if ( line.empty() ) {
+                throw runtime_error( filename + ": invalid empty line" );
             }
+
+            const uint64_t ms = myatoi( line );
+
+            if ( not schedule_.empty() ) {
+                if ( ms < schedule_.back() ) {
+                    throw runtime_error( filename + ": timestamps must be monotonically nondecreasing" );
+                }
+            }
+
+            schedule_.emplace_back( ms );
         }
 
-        schedule_.emplace_back( ms );
-    }
+        if ( schedule_.empty() ) {
+            throw runtime_error( filename + ": no valid timestamps found" );
+        }
 
-    if ( schedule_.empty() ) {
-        throw runtime_error( filename + ": no valid timestamps found" );
-    }
-
-    if ( schedule_.back() == 0 ) {
-        throw runtime_error( filename + ": trace must last for a nonzero amount of time" );
+        if ( schedule_.back() == 0 ) {
+            throw runtime_error( filename + ": trace must last for a nonzero amount of time" );
+        }
     }
 
     /* open logfile if called for */
@@ -114,10 +156,16 @@ void LinkQueue::record_arrival( const uint64_t arrival_time, const size_t pkt_si
     if ( throughput_graph_ ) {
         throughput_graph_->add_value_now( 1, pkt_size );
     }
+
+    bytes_ingress_ += pkt_size;
+    packets_ingress_ += 1;
 }
 
 void LinkQueue::record_drop( const uint64_t time, const size_t pkts_dropped, const size_t bytes_dropped)
 {
+    packet_drops_ += 1;
+    bytes_drops_ += bytes_dropped;
+
     /* log it */
     if ( log_ ) {
         *log_ << time << " d " << pkts_dropped << " " << bytes_dropped << endl;
@@ -153,11 +201,16 @@ void LinkQueue::record_departure( const uint64_t departure_time, const QueuedPac
     if ( delay_graph_ ) {
         delay_graph_->set_max_value_now( 0, departure_time - packet.arrival_time );
     }    
+
+    bytes_egress_ += packet.contents.size();
+    packets_egress_ += 1;
 }
 
 void LinkQueue::read_packet( const string & contents )
 {
     const uint64_t now = timestamp();
+    static std::default_random_engine generator;
+    static std::uniform_real_distribution<double> distro(0.0,1.0);
 
     if ( contents.size() > PACKET_SIZE ) {
         throw runtime_error( "packet size is greater than maximum" );
@@ -166,6 +219,11 @@ void LinkQueue::read_packet( const string & contents )
     rationalize( now );
 
     record_arrival( now, contents.size() );
+
+    if (distro(generator) <= cur_loss_rate_) {
+        record_drop( now, 1, contents.size() );
+        return;
+    }
 
     unsigned int bytes_before = packet_queue_->size_bytes();
     unsigned int packets_before = packet_queue_->size_packets();
@@ -187,22 +245,61 @@ uint64_t LinkQueue::next_delivery_time( void ) const
     if ( finished_ ) {
         return -1;
     } else {
-        return schedule_.at( next_delivery_ ) + base_timestamp_;
+        if (is_adversary_) {
+            return base_timestamp_ + bandwidth_history;
+        } else {
+            return schedule_.at( next_delivery_ ) + base_timestamp_;
+        }
     }
+}
+
+int LinkQueue::read_next_action() {
+    int ret = poll(poll_fds_, 1, 0);
+    if (!(ret == 1 && (poll_fds_[0].revents & POLLIN))) {
+        return -1;
+    }
+
+    static char buf[1024];
+    int result = read(poll_fds_[0].fd, buf, 1024);
+	assert(result > 0);
+    char* end = &(buf[0]);
+	double action_bw = strtod(buf, &end);
+	end++;
+	double action_delay = strtod(end, &end);
+    end++;
+    double action_loss_rate = strtod(end, &end);
+    LinkQueueAction lqa(action_bw, action_delay, action_loss_rate);
+    actions_.push(lqa);
+
+    return 0;
 }
 
 void LinkQueue::use_a_delivery_opportunity( void )
 {
     record_departure_opportunity();
 
-    next_delivery_ = (next_delivery_ + 1) % schedule_.size();
+    if (is_adversary_) {
+        // stay "silent" for ```total_bandwidth_cycles - total_delivery_cycles``` milliseconds
+        // then send each millesecond for ```total_delivery_cycles```
+        
+        if (next_delivery_ < total_bandwidth_cycles - total_delivery_cycles) { 
+            next_delivery_ = total_bandwidth_cycles - total_delivery_cycles; 
+            bandwidth_history += total_bandwidth_cycles - total_delivery_cycles; 
+        } 
+        
+        bandwidth_history++;
+        
+        next_delivery_ = (next_delivery_ + 1) % total_bandwidth_cycles; 
+    } else {
+        next_delivery_ = (next_delivery_ + 1) % schedule_.size();
 
-    /* wraparound */
-    if ( next_delivery_ == 0 ) {
-        if ( repeat_ ) {
-            base_timestamp_ += schedule_.back();
-        } else {
-            finished_ = true;
+        /* wraparound */
+        if ( next_delivery_ == 0 ) {
+            if ( repeat_ ) {
+                base_timestamp_ += schedule_.back();
+            } else {
+                finished_ = true;
+            }
         }
     }
 }
@@ -212,6 +309,54 @@ void LinkQueue::use_a_delivery_opportunity( void )
    calculating the wait_time until the next event */
 void LinkQueue::rationalize( const uint64_t now )
 {
+
+    if (is_adversary_) {
+
+        mi_start_queue_size_ = packet_queue_->size_bytes();
+
+        if (read_next_action() == 0) {
+            adversary_data_out_
+                << mi_start_time_ << ","
+                << now << ","
+                << cur_bw_ << ","
+                << cur_delay_ms_ << ","
+                << cur_loss_rate_ << ","
+                << bytes_ingress_ << ","
+                << packets_ingress_ << ","
+                << bytes_egress_ << ","
+                << packets_egress_ << ","
+                << packet_drops_ << ","
+                << bytes_drops_ << ","
+                << total_delay_ << ","
+                << mi_start_queue_size_ << ","
+                << packet_queue_->size_bytes() << std::endl;
+
+            mi_start_time_ = now;
+            packets_ingress_ = 0;
+            bytes_ingress_ = 0;
+            packets_egress_ = 0;
+            bytes_egress_ = 0;
+            packet_drops_ = 0;
+            bytes_drops_ = 0;
+            total_delay_ = 0;
+        }
+
+        if (!actions_.empty()) {
+            LinkQueueAction lqa = actions_.front();
+            actions_.pop();
+            // round up the bandwidth up to 1 digit (in Mbps)
+            cur_bw_ = (floorf(lqa.bw * 10) / 10) * 1000000.0;
+            cur_delay_ms_ = (int)lqa.delay_ms;
+            cur_loss_rate_ = lqa.loss_rate;
+
+            // Used for bandwidths that are not multiples of 12.
+            // For example, for 8 Mbps, LCM(12, 8)=24. So we need to have 24/12=2 deliveries out of 24/8=3.
+            lcm = std::lcm(12000000, (int64_t) round(cur_bw_));
+            total_delivery_cycles = lcm / 12000000;
+            total_bandwidth_cycles = lcm / (int64_t) round(cur_bw_);
+        }
+    }
+
     while ( next_delivery_time() <= now ) {
         const uint64_t this_delivery_time = next_delivery_time();
 
@@ -227,7 +372,11 @@ void LinkQueue::rationalize( const uint64_t now )
                 packet_in_transit_ = packet_queue_->dequeue();
                 packet_in_transit_bytes_left_ = packet_in_transit_.contents.size();
             }
-
+            
+            if (packet_in_transit_.arrival_time > this_delivery_time) {
+                std::cerr << "Delivery time: " << this_delivery_time << std::endl;
+                std::cerr << "Arrival time: " << packet_in_transit_.arrival_time << std::endl;
+            }
             assert( packet_in_transit_.arrival_time <= this_delivery_time );
             assert( packet_in_transit_bytes_left_ <= PACKET_SIZE );
             assert( packet_in_transit_bytes_left_ > 0 );
@@ -243,21 +392,49 @@ void LinkQueue::rationalize( const uint64_t now )
 
             /* has the packet been fully sent? */
             if ( packet_in_transit_bytes_left_ == 0 ) {
-                record_departure( this_delivery_time, packet_in_transit_ );
+                const uint64_t departure_time = get_departure_time(this_delivery_time);
+                record_departure(departure_time, packet_in_transit_);
+
+                total_delay_ += departure_time - packet_in_transit_.arrival_time;
 
                 /* this packet is ready to go */
-                output_queue_.push( move( packet_in_transit_.contents ) );
+                TransitPacket tp(departure_time, 
+                    packet_in_transit_.contents);
+                transit_queue_.push(tp);
             }
         }
     }
 }
 
+uint64_t LinkQueue::get_departure_time(uint64_t delivery_time) {
+    if (!is_adversary_) {
+        return delivery_time;
+    }
+    
+    uint64_t departure_time = delivery_time + cur_delay_ms_;
+
+    if (departure_time < prev_departure_time_) {
+        departure_time = prev_departure_time_;
+    } else {
+        prev_departure_time_ = departure_time;
+    }
+
+    return departure_time;
+}
+
 void LinkQueue::write_packets( FileDescriptor & fd )
 {
+    while(!transit_queue_.empty()
+        && transit_queue_.top().exit_time <= timestamp()) {
+        fd.write(transit_queue_.top().contents);
+        transit_queue_.pop();
+    }
+    /*
     while ( not output_queue_.empty() ) {
         fd.write( output_queue_.front() );
         output_queue_.pop();
     }
+    */
 }
 
 unsigned int LinkQueue::wait_time( void )
@@ -275,5 +452,12 @@ unsigned int LinkQueue::wait_time( void )
 
 bool LinkQueue::pending_output( void ) const
 {
-    return not output_queue_.empty();
+    if (transit_queue_.empty()) {
+        return false;
+    }
+    if (is_adversary_) {
+        return transit_queue_.top().exit_time <= timestamp();
+    } else {
+        return true;
+    }
 }
--- src/frontend/link_queue.hh
+++ src/frontend/link_queue.hh
@@ -9,9 +9,40 @@
 #include <fstream>
 #include <memory>
 
+#include <poll.h>
+
 #include "file_descriptor.hh"
 #include "binned_livegraph.hh"
 #include "abstract_packet_queue.hh"
+
+struct TransitPacket {
+    TransitPacket(uint64_t exit_time, std::string contents)
+        : exit_time(exit_time),
+          contents(move(contents)) {
+    };
+    uint64_t exit_time;
+    std::string contents;
+};
+
+class TransitPacketCompare {
+  public:
+    bool operator() (const TransitPacket& p1, const TransitPacket& p2) {
+        return p1.exit_time > p2.exit_time;
+    }
+};
+
+struct LinkQueueAction {
+    LinkQueueAction(double bw, double delay_ms, double loss_rate)
+        : bw(bw),
+          delay_ms(delay_ms),
+          loss_rate(loss_rate) {
+
+    }
+    double bw;
+    double delay_ms;
+    double loss_rate;
+};
+
 
 class LinkQueue
 {
@@ -21,11 +52,43 @@ private:
     unsigned int next_delivery_;
     std::vector<uint64_t> schedule_;
     uint64_t base_timestamp_;
+    mutable uint64_t bandwidth_history;
+
+    bool is_adversary_;
+    double cur_bw_;
+    uint64_t cur_delay_ms_;
+    double cur_loss_rate_;
+    uint64_t packets_this_departure_time_;
+    uint64_t prev_departure_time_;
+    uint64_t next_possible_departure_time_;
+
+    long lcm;
+    long total_bandwidth_cycles;
+    long total_delivery_cycles;
+
+    uint64_t packets_ingress_;
+    uint64_t bytes_ingress_;
+    uint64_t packets_egress_;
+    uint64_t bytes_egress_;
+    uint64_t packet_drops_;
+    uint64_t bytes_drops_;
+    uint64_t total_delay_;
+    uint64_t mi_start_time_;
+    uint64_t mi_start_queue_size_;
+    uint64_t mi_dur_;
+
+    std::queue<LinkQueueAction> actions_;
+    bool got_valid_action_;
+
+    struct pollfd poll_fds_[1];
 
     std::unique_ptr<AbstractPacketQueue> packet_queue_;
     QueuedPacket packet_in_transit_;
     unsigned int packet_in_transit_bytes_left_;
-    std::queue<std::string> output_queue_;
+
+    std::priority_queue<TransitPacket, std::vector<TransitPacket>, TransitPacketCompare> transit_queue_;
+
+    std::ofstream adversary_data_out_;
 
     std::unique_ptr<std::ofstream> log_;
     std::unique_ptr<BinnedLiveGraph> throughput_graph_;
@@ -42,9 +105,12 @@ private:
     void record_drop( const uint64_t time, const size_t pkts_dropped, const size_t bytes_dropped );
     void record_departure_opportunity( void );
     void record_departure( const uint64_t departure_time, const QueuedPacket & packet );
+    uint64_t get_departure_time(uint64_t delivery_time);
 
     void rationalize( const uint64_t now );
     void dequeue_packet( void );
+
+    int read_next_action();
 
 public:
     LinkQueue( const std::string & link_name, const std::string & filename, const std::string & logfile,
